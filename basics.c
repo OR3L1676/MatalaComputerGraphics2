73bab6af9c158298a77a85f957d648a97755faaa
OR3L1676 <104456513+OR3L1676@users.noreply.github.com>
2024-02-16 12:23:00 +0200

    Update basics.c

--- basics.c
+++ basics.c
@@ -1,5 +1,29 @@
 
-// - מגיש אוראל סבש
+/* basics 
+ * 
+ * A basic, first OpenGL program.
+ * 
+ * This program illustrates how you open an window, specify its position and size,
+ * and draw something in the window.
+ *
+ * Also illustrated: the state-machine operation of OGL. Notice that if you resize
+ * the window, the box becomes blue -- the last color specified! This isn't the right
+ * idea! Where should you put the color of the box?
+ *
+ * When you resize the window so it is not square, the box changes shape. This might
+ * not be desired -- we will look into fixing this later. (Keep in mind, this has
+ * something to do with the aspect ratio.)
+ *
+ * Try changing the size of gluOrtho2D -- see in code below -- and you'll see how the
+ * relative size of the box changes within the window.
+ *
+ * See http://www.eas.asu.edu/~cse470/references.html for instruction on how to start 
+ * your own program 
+ *
+ * Dianne Hansford, August 2004
+ */
+
+
 #include <stdio.h>
 
 /* Include the GLUT library. This file (glut.h) contains gl.h and glu.h */
@@ -19,35 +43,12 @@ void display()
 	/* clear the screen*/
 	glClear(GL_COLOR_BUFFER_BIT);
 
-
-	// - מרובע אדום
+	/* draw a filled polygon */
 	glBegin(GL_POLYGON);
-		glColor3f(1.0f, 0.0f, 0.0f); // - שינוי לצבע אדום
 		glVertex2f(-0.5, -0.5);
 		glVertex2f( 0.5, -0.5);
 		glVertex2f( 0.5,  0.5);
 		glVertex2f(-0.5,  0.5);
-	glEnd();
-	
-	// - משולש ירוק
-	glBegin(GL_POLYGON);
-	glColor3f(0.0f, 1.0f, 0.0f); // - שינוי לצבע ירוק
-	glVertex2f(-2.0, -0.5);
-	glVertex2f(-1.0, -0.5);
-	glVertex2f(-1.5, 0.5);
-	glEnd();
-
-	// - משושה צהוב
-	glBegin(GL_POLYGON);
-	glColor3f(1.0f, 1.0f, 0.0f); // - שינוי לצבע צהוב
-	glVertex2f(1.5, -0.5);
-	glVertex2f(2.0, -0.5);
-	glVertex2f(2.25, 0.0);
-	glVertex2f(2.0, 0.5);
-	glVertex2f(1.5, 0.5);
-	glVertex2f(1.25, 0.0);
-
-
 	glEnd();
 
 	/* draw a fancy line -- red to blue variation in color */
@@ -58,7 +59,6 @@ void display()
 		glColor3f(0.0, 0.0, 1.0);
 		glVertex2f(0.75, -0.75);
 	glEnd();
-
 
 	/* ready to draw now! forces buffered OGL commands to execute */
 	glFlush();
@@ -84,7 +84,7 @@ void init()
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	/* the window will correspond to these world coorinates */
-	gluOrtho2D(-5.0, 5.0, -5.0, 5.0); // - שינוי מיקום החלון 
+	gluOrtho2D(-1.0, 1.0, -1.0, 1.0);
 
 	/* we can play ... see what happens if we swap out this one with that above */
 	//gluOrtho2D(-10.0, 10.0, -10.0, 10.0);
@@ -112,8 +112,8 @@ int main(int argc, char** argv)
 	/* create a window, position it, and name it */
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
-	glutInitWindowSize(750, 750); // - שינוי גודל חלון
-	glutInitWindowPosition(400, 250); // - שינוי מיקום חלון
+	glutInitWindowSize(500, 500);
+	glutInitWindowPosition(200, 200);
 	glutCreateWindow("basics");
 
 	fprintf(infofile,"Created windows\n");
